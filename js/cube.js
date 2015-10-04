3b03d48c86af8a88d477a8f8dd465abb18bfb9b6
Nat Zimmermann <nat@natzim.me>
2015-10-04 23:50:41 +0100

    Clean up JS file

--- js/cube.js
+++ js/cube.js
@@ -1,3 +1,9 @@
+// ---
+// LED Cube JS
+//
+// @author Nat Zimmermann <hi@natzim.xyz>
+// ---
+
 var settings = {
     cube: {
         size: 8,
@@ -14,6 +20,10 @@ var settings = {
     }
 };
 
+// ---
+// Classes
+// ---
+
 var Led = function(object) {
     this.object = object;
 };
@@ -26,43 +36,104 @@ Led.prototype.setColor = function(hex) {
     this.object.material.color.setHex(hex);
 };
 
-document.getElementById('led-settings').addEventListener('submit', function(e) {
+// ---
+// Functions
+// ---
+
+function generateCube(size) {
+    for (var i = 0; i < Math.pow(size, 3); i++) {
+        // Construct LED geometry
+        var geometry = new THREE.BoxGeometry(
+            settings.cube.leds.size,
+            settings.cube.leds.size,
+            settings.cube.leds.size
+        );
+
+        // Construct LED material
+        var material = new THREE.MeshBasicMaterial({
+            wireframe: true,
+            color: settings.cube.leds.defaultColor,
+            opacity: settings.cube.leds.opacity
+        });
+
+        // Construct LED mesh
+        var led = new THREE.Mesh(geometry, material);
+
+        // Calculate co-ordinates
+        var x = Math.floor(i / size) % size;
+        var y = Math.floor(i / Math.pow(size, 2));
+        var z = i % size;
+
+        led.position.set(x, y, z);
+
+        // Generate 3D array on the fly
+        if (!leds[x]) {
+            leds[x] = [];
+        }
+        if (!leds[x][y]) {
+            leds[x][y] = [];
+        }
+
+        // Store new LED in array
+        leds[x][y][z] = new Led(led);
+
+        // Add LED to the scene
+        scene.add(led);
+    }
+}
+
+function setLedOptions(e) {
     e.preventDefault();
 
-    var x = document.getElementById('x').value;
-    var y = document.getElementById('y').value;
-    var z = document.getElementById('z').value;
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var z = $('#z').val();
 
-    var color = parseInt(document.getElementById('color').value, 16);
+    var color = parseInt($('#color').val(), 16);
 
     leds[x][y][z].setColor(color);
-});
+}
 
-function updateLedSettings() {
-    var x = document.getElementById('x').value;
-    var y = document.getElementById('y').value;
-    var z = document.getElementById('z').value;
+function showLedOptions() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var z = $('#z').val();
 
     var hex = leds[x][y][z].getColor().toString(16).toUpperCase();
     hex = "000000".substr(0, 6 - hex.length) + hex;
 
-    document.getElementById('color').value = hex;
+    $('#color').val(hex);
 }
 
-document.getElementById('x').addEventListener('change', updateLedSettings);
-document.getElementById('y').addEventListener('change', updateLedSettings);
-document.getElementById('z').addEventListener('change', updateLedSettings);
+// ---
+// Event listeners
+// ---
 
+$('#led-settings').submit(setLedOptions);
+$('#x, #y, #z').change(showLedOptions);
+
+// ---
+// Main
+// ---
+
+// Calculate center co-ordinates
+// This is used for the center of focus for the camera
 var center = settings.cube.size / 2 - 0.5;
 var centerVector = new THREE.Vector3(center, center, center);
 
+// Generate scene
 var scene = new THREE.Scene();
+
+// Generate camera and set position
 var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 camera.position.copy(centerVector);
 camera.position.z *= 4;
 
-var renderer = new THREE.WebGLRenderer({alpha: false});
+// Generate renderer and set canvas size
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(settings.canvas.width, settings.canvas.height);
+
+// Add canvas to DOM
 settings.canvas.parentElement.appendChild(renderer.domElement);
 
 // Enable camera controls
@@ -70,42 +141,17 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.enableKeys = false;
 controls.target.copy(centerVector);
-
 controls.update();
 
+// 3D array to store LEDs in their [x][y][z] co-ordinates
 var leds = [];
 
-// Generate leds
-for (var i = 0; i < Math.pow(settings.cube.size, 3); i++) {
-    var geometry = new THREE.BoxGeometry(settings.cube.leds.size, settings.cube.leds.size, settings.cube.leds.size);
-    var material = new THREE.MeshBasicMaterial({
-        wireframe: true,
-        color: settings.cube.leds.defaultColor,
-        opacity: settings.cube.leds.opacity
-    });
-    var led = new THREE.Mesh(geometry, material);
+generateCube(settings.cube.size);
+showLedOptions();
 
-    var x = Math.floor(i / settings.cube.size) % settings.cube.size;
-    var y = Math.floor(i / Math.pow(settings.cube.size, 2));
-    var z = i % settings.cube.size;
-
-    led.position.set(x, y, z);
-
-    // Generate 3D array on the fly
-    if (!leds[x]) {
-        leds[x] = [];
-    }
-
-    if (!leds[x][y]) {
-        leds[x][y] = [];
-    }
-
-    leds[x][y][z] = new Led(led);
-
-    scene.add(led);
-}
-
-updateLedSettings();
+// ---
+// Render loop
+// ---
 
 function render() {
     requestAnimationFrame(render);
